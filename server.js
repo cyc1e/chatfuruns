8d867d2e3a77a5615ad11c6458a3ef87760f454e
cyc1e <petroprom76@gmail.com>
2018-05-12 14:19:56 +0300

    server js in derictory

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const cookieParser = require('cookie-parser');
 const passport = require('passport');
 const { Strategy } = require('passport-jwt');
 
-const { jwt } = require('./config');
+const { jwt } = require('./server/config');
 
 passport.use(new Strategy(jwt, function(jwt_payload, done) {
     if(jwt_payload != void(0)) return done(false, jwt_payload);
@@ -53,9 +53,9 @@ app.use(bodyParser.json());
 
 app.use(cookieParser());
 
-require('./router')(app);
+require('./server/router')(app);
 
-require('./sockets')(io);
+require('./server/sockets')(io);
 
 server.listen(7777, () => {
     console.log('Server started on port 7777');
