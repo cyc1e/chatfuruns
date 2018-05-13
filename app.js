0568af2f5b629a09546c1067d17a898d26bbd9e6
cyc1e <petroprom76@gmail.com>
2018-05-13 11:33:23 +0300

    run

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ const io = require('socket.io')(server, {serveClient: true});
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const PORT = process.env.PORT || 7777
+// const PORT = process.env.PORT || 7777
+
+const PORT = 7777
+if (process.env.NODE_ENV === 'production') {
+    PORT = "https://chatfuruns.herokuapp.com";
+}
+
 
 const passport = require('passport');
 const { Strategy } = require('passport-jwt');
